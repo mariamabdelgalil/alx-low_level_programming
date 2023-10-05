2044f1c88471dfca0601e791bf89ded3f669979e
mariamabdelgalil <mariam.ahmad.200300@gmail.com>
2023-10-05 08:26:32 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: 0 in success
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+	return (0);
+}
